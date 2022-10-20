f616e9b7d4cc8623022345932c794604b9ef8203
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-19 21:33:20 -0400

    Navigating class hierarchies

--- NavigatingClassHierarchy/main.cpp
+++ NavigatingClassHierarchy/main.cpp
@@ -1,14 +1,27 @@
-//
-//  main.cpp
-//  NavigatingClassHierarchy
-//
-//  Created by Matt Ahmad on 2022-10-19.
-//
-
 #include <iostream>
+#include <typeinfo>
+using namespace std;
+
+class Shape { public: virtual ~Shape() {}; };
+class Circle : public Shape {};
+class Square : public Shape {};
+class Other {};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    Circle c;
+    Shape* s = &c;   //Upcast
+    s = static_cast<Shape*>(&c); // More explicit bu unnecessary
+    Circle* cp = 0;
+    Square* sp = 0;
+    //Static Nav of class hierarchies
+    //requires extra type information:
+    if(typeid(s) == typeid(cp))
+        cp = static_cast<Circle*>(s);
+    if(typeid(s) == typeid(sp))
+        sp = static_cast<Square*>(s);
+    if(cp != 0)
+        cout << "It's a circle!" << endl;
+    if(sp != 0)
+        cout << "It's a square!" << endl;
 }
